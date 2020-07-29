882ca64504fc45be674caa8cbd4a0fde32f48979
keng42 <me@keng42.com>
2020-07-29 11:39:48 +0800

    chore: update build-protos

--- scripts/build-protos.sh
+++ scripts/build-protos.sh
@@ -11,9 +11,9 @@ mkdir -p ${PROTO_DEST}
 
 # JavaScript code generation
 npx grpc_tools_node_protoc \
+    --plugin=protoc-gen-grpc=./node_modules/.bin/grpc_tools_node_protoc_plugin \
     --js_out=import_style=commonjs,binary:${PROTO_DEST} \
     --grpc_out=${PROTO_DEST} \
-    --plugin=protoc-gen-grpc=./node_modules/.bin/grpc_tools_node_protoc_plugin \
     -I ./proto \
     proto/*.proto
 
